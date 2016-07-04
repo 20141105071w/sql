26cfd55f3d980d268bab53ff3ecbe13cd8cdd199
20141105071 <20141105071wyr@mac26.local>
2016-07-04 11:38:32 +0800

    telephone book
    首先，在iOS页面中添加按钮，分别是添加联系人、删除联系人、查看联系人，然后在添加一个iOS页面，与添加联系人关联，在这个页面中添加标签与编辑框
    ，并添加保存按钮。做好以上工作后，在Build Phases -> Link Binary With
    Libraries中点击加号，添加libsqlite3.0.tdb到项目中来
    ，创建连接头文件Bridging-Header.h，在项目编译属性里引用头文件，
    导入SQLiteDB的代码（SQLiteDB.swift、SQLTable.swift和String-Extras.swift），运行可实现将数
    据输入并保存的功能，

---
+++ sql/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  sql
+//
+//  Created by 20141105071 on 16/7/4.
+//  Copyright © 2016年 20141105071. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+#import "sqlite3.h"
+#import <time.h>
+
+#endif /* Header_h */
